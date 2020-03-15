dd962d9c55d5f306a65f514ca7a28a0be7d86d72
Alegi <ale.giannetti@hotmail.it>
2020-03-15 17:31:32 +0100

    Spectrum generation with delta or for all channels in wav

--- Spectrum.py
+++ Spectrum.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 
+plt.ioff()
 
 class Spectrum:
 
@@ -28,7 +29,7 @@ class Spectrum:
         return fig
 
     @staticmethod
-    def get_specgram_mplib(path, fmt="svg", NFFT=1024, noverlap=512):
+    def get_specgram_mplib(path, file_name, out_path, fmt="svg", NFFT=1024, noverlap=512):
         """
         :param path: str
             path to the .wav file to generate spectrogram
@@ -43,9 +44,10 @@ class Spectrum:
             creates a image into the path with the same name of input file
             If the data has more channels, creates more svg files.
         """
+
         out_name = path
         if path.endswith(".wav"):
-            out_name = path[:-4]
+            out_name = out_path+file_name
         rate, data = wavfile.read(path)
         if data.ndim > 1:
             # we have more than one channel, we have to do more plots
@@ -54,13 +56,15 @@ class Spectrum:
                 out_path = out_name + "_channel_" + str(i) + "." + fmt
                 fig = Spectrum.plot_spectrogram(rate, dimension_data, NFFT, noverlap);
                 fig.savefig(out_path, format=fmt, frameon='false')
+                plt.close(fig)
         else:
             out_name += "." + fmt
             fig = Spectrum.plot_spectrogram(rate, data, NFFT, noverlap);
             fig.savefig(out_name, format=fmt, frameon='false')
+            plt.close(fig)
 
     @staticmethod
-    def compute_specgram_and_delta(path, sample_rate=44100, normalize=True, nfft=1024, hop_len=512, n_mel_bands=60):
+    def compute_specgram_and_delta(path, sample_rate=22050, normalize=True, nfft=1024, hop_len=512, n_mel_bands=60):
         """
         :param path: str
             path where the wav file is located
@@ -93,7 +97,7 @@ class Spectrum:
         return np.stack([log_spec, delta_log_spec], axis=-1)
 
     @staticmethod
-    def get_specgram_librosa(path, fmt='png', which='both', sample_rate=44100, nfft=1024, hop_len=512, n_mel_bands=60):
+    def get_specgram_librosa(path, file_name, out_path, fmt='png', which='both', sample_rate=22050, nfft=1024, hop_len=512, n_mel_bands=60):
         """
         :param path: str
             path where the wav file is located
@@ -115,7 +119,7 @@ class Spectrum:
         """
         out_name = path
         if path.endswith(".wav"):
-            out_name = path[:-4]
+            out_name = out_path+file_name
 
         spec = Spectrum.compute_specgram_and_delta(path, sample_rate, nfft, hop_len, n_mel_bands)
         log_spec = spec[:, :, 0]
@@ -131,3 +135,50 @@ class Spectrum:
             out_path = out_name + '_delta.' + fmt
             plt.savefig(out_path, format=fmt, frameon='false', bbox_inches='tight', pad_inches=0)
 
+
+# fft - fast fourier transform
+# =============================================================================
+def main():
+    # generate spectograms from segments
+
+    audio_path = '../segments/'
+
+    all_segments = os.listdir(audio_path)
+    all_segments.sort()
+    print(all_segments)
+
+    for directory in all_segments:
+
+        audio_path = '../segments/' + directory
+        img_path = '../segments-spectogram/' + directory
+
+        all_category = os.listdir(audio_path)
+        all_category.sort()
+        print(all_category)
+
+        for category in all_category:
+
+            all_sub_category = os.listdir(audio_path + '/' + category)
+            all_sub_category.sort()
+            print(all_sub_category)
+
+            for sub_category in all_sub_category:
+                print(directory + '/' + category + '/' + sub_category)
+                all_tracks = os.listdir(audio_path + '/' + category + '/' + sub_category + '/')
+
+                for track in all_tracks:
+                    sys.stdout.write('\r' + track)
+                    sys.stdout.flush()
+
+                    wav_path = audio_path + '/' + category + '/' + sub_category + '/' + track
+                    spectogram_path = img_path + '/' + category + '/' + sub_category + '/'
+                    #generation of spectrogram for all channels in wav
+                    #Spectrum.get_specgram_mplib(wav_path, track[:-4], spectogram_path)
+
+                    #generation of spectrum based on delta
+                    Spectrum.get_specgram_librosa(wav_path, track[:-4], spectogram_path)
+
+
+if __name__ == "__main__":
+    main()
+# =============================================================================
